763ad722776737dc5cd89152311f9eb414b86daa
Mac <7kuz@mail.ru>
2025-03-12 16:59:38 +0300

    И далее pandas нагружаем

--- mod_pandas.py
+++ mod_pandas.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
 import kagglehub
 
 series = pd.Series([10, 20,30, 40, 50])
@@ -13,4 +16,17 @@ df = pd.DataFrame({'A': [1, 4],
                    'B': [2, 5],
                    'C': [3, 6]})
 
-path = kagglehub.read_csv()
+#path = kagglehub.read_csv()
+data = pd.Series([0, np.nan, 100])
+print(data.mean())
+
+x = [1, 2, 3, 4, 5]
+y = [10, 20, np.nan, 40, 50]
+
+plt.plot(x, y, marker='o', color='red', linestyle='--')
+plt.title('Graph with missing data')
+#plt.show()
+
+#titanic = pd.read_csv("https://github.com/mwaskom/seaborn-data/blob/master/titanic.csv")
+#titanic.head()
+
